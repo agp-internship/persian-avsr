a7b7a1df8a9c97f9189b52e3df35ad0521c783ae
Amir01 <iran.volleyball79@gmail.com>
2022-08-03 17:05:45 +0430

    rename sel.py to main.py and add argument parser

---
+++ data-collection/Babaei/main.py
@@ -0,0 +1,13 @@
+import sys
+from scraper.telewebion import TelewebionScraper
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Script to scrape telewebion archive videos')
+
+parser.add_argument('-d', '--days', action='store', default=1, type=int, help='how many days scrape by script since today to the past')
+parser.add_argument('-c', '--channel', action='store', default='irinn', help='channel that you want to scrape its archive')
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    with TelewebionScraper() as tele_obj:
+        tele_obj.run(days=args.days, channel=args.channel)
--- data-collection/Babaei/sel.py
+++
@@ -1,5 +0,0 @@
-from scraper.telewebion import TelewebionScraper
-
-if __name__ == "__main__":
-    with TelewebionScraper() as tele_obj:
-        tele_obj.run(days=2)  
