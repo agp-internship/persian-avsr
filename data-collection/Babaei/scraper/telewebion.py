941c04b4aa192791c993682808077b97b730eeb6
Amir01 <iran.volleyball79@gmail.com>
2022-08-15 20:20:32 +0430

    add metadata extraction and decrease load-more button waiting

--- data-collection/Babaei/scraper/telewebion.py
+++ data-collection/Babaei/scraper/telewebion.py
@@ -1,11 +1,13 @@
-from logging import getLogger
 import os
-from sys import exc_info
 import time
 import requests
 import jdatetime
 from tqdm import tqdm
+from logging import getLogger
+from argparse import Namespace
 from dotenv import load_dotenv
+from datetime import datetime, timezone
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.keys import Keys
@@ -14,6 +16,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchWindowException, InvalidSessionIdException
 from exceptions.exceptions import ChannelDoesNotExistException
+
 from logger.logger import logger_config
 
 class TelewebionScraper(webdriver.Firefox):
@@ -24,6 +27,7 @@ class TelewebionScraper(webdriver.Firefox):
     def __init__(self, webdriver_path='./geckodriver/geckodriver.exe', close_browser=True):
         logger_config()
         self.logger = getLogger(__name__)
+        self.elements = None
         self.download_dict = dict()
         self.download_quality = ['480', '720', '1080']
         self.valid_channels = self.load_valid_channels()
@@ -32,7 +36,7 @@ class TelewebionScraper(webdriver.Firefox):
         super(TelewebionScraper, self).__init__(service=self.service)
         self.implicitly_wait(20)
         self.maximize_window()
-        self.logger.info(f'Firefox driver path: {webdriver_path}')
+        self.logger.info(f'Firefox driver path: {os.path.abspath(webdriver_path)}')
         self.logger.info('TelewebionScraper object initialized.')
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -81,18 +85,50 @@ class TelewebionScraper(webdriver.Firefox):
             self.logger.info('There is no load-more button.')
             return False
 
-    def get_episodes(self):
-        elems = WebDriverWait(self, 20).until(
-            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'a[href*="/episode/"]'))
+    def get_episodes(self, date, channel):
+        app_cards = WebDriverWait(self, 20).until(
+            EC.visibility_of_all_elements_located((By.TAG_NAME, 'app-card-cover-episode'))
         )
-        elems = list(set([elem.get_attribute('href') for elem in elems])) # remove duplicate links
-        self.logger.info(f"{len(elems)} episodes were found.")
-        return elems
 
-    def extract_link(self, elem): 
+        elems_duration = [
+            app_card.find_element(By.CSS_SELECTOR, 'div.col-5 > span')
+            .get_attribute('innerHTML') 
+            for app_card in app_cards
+        ]
+        info_frames = [
+            app_card.find_element(By.CLASS_NAME, 'info-frame')
+            for app_card in app_cards
+        ]
+
+        elems_episodes = []
+        elems_programs = []
+
+        for frame in info_frames:
+            elems_episodes.append(frame.find_element(By.CSS_SELECTOR, 'a[href*="/episode/"]'))
+            elems_programs.append(frame.find_element(By.CSS_SELECTOR, 'a[href*="/program/"]'))
+
+        elems_links = [elem.get_attribute('href') for elem in elems_episodes]
+        elems_title = [elem.get_attribute('title') for elem in elems_episodes]
+        elems_program = [elem.get_attribute('title') for elem in elems_programs]
+        
+        self.logger.info(f"{len(elems_links)} episodes were found.")
+
+        self.elements = [
+            Namespace(
+                link=elems_links[i],
+                title=elems_title[i],
+                program_name=elems_program[i],
+                duration=elems_duration[i], 
+                channel=channel,
+                date=date
+            )
+            for i in range(len(elems_links))
+        ]
+
+    def extract_link(self, link): 
         try:
-            self.get(elem)
-            self.logger.info(f'request sent to {elem}')
+            self.get(link)
+            self.logger.info(f'request sent to {link}')
             self.implicitly_wait(20)
 
             # play_elem = WebDriverWait(driver, 20).until(
@@ -114,7 +150,7 @@ class TelewebionScraper(webdriver.Firefox):
             for i in range(len(download_items)):
                 download_links[self.download_quality[i]] = download_items[i].find_element(By.CSS_SELECTOR, 'a[href*="dl.telewebion.com"]').get_attribute('href')
 
-            self.download_dict[elem] = download_links
+            self.download_dict[link] = download_links
             self.logger.info('Download links extracted.')
 
             time.sleep(2)
@@ -129,15 +165,19 @@ class TelewebionScraper(webdriver.Firefox):
     def get_link_per_channel_date(self, date, channel):
         self.get_archive(date, channel)
         self.set_cookie_auth()
+
         load_more_button_exist = True
+        self.implicitly_wait(2)
         while load_more_button_exist:
             load_more_button_exist = self.click_load_more_button()
-            time.sleep(0.5)
-        elems = self.get_episodes()
-        for elem in tqdm(elems):
-            self.extract_link(elem)
+            time.sleep(0.1)
+        self.implicitly_wait(20)
 
-    def write_to_file(self):
+        self.get_episodes(date, channel)
+        for elem in tqdm(self.elements):
+            self.extract_link(elem.link)
+
+    def write_links(self):
         os.makedirs('./data/', exist_ok=True)
         f_480   = open('./data/480.txt', 'a')
         f_720   = open('./data/720.txt', 'a')
@@ -155,14 +195,39 @@ class TelewebionScraper(webdriver.Firefox):
         self.logger.info('Extracted links wrote to files.')
         self.download_dict.clear()
 
+    def write_metadata(self):
+        for elem in self.elements:
+            video_id = elem.link.split('/')[-1]
+            dir_name = f'./data/videos/{video_id}'
+            os.makedirs(dir_name, exist_ok=True)
+
+            filename = f'{dir_name}/{video_id}-metadata.txt'
+            with open(filename, 'w') as metadata:
+                meta_string = ''
+                meta_string += f'Video ID: {video_id}\n'
+                meta_string += f'Video Link: {elem.link}\n'
+                meta_string += f'Video Title: {elem.title}\n'
+                meta_string += f'Program Name: {elem.program_name}\n'
+                meta_string += f'Video Duration: {elem.duration}\n'
+                meta_string += f'Channel: {elem.channel}\n'
+                meta_string += f'Extract Date (Jalali): {elem.date.strftime("%a, %d %b %Y")}\n'
+                meta_string += f'Extract Date (Gregorian): {elem.date.togregorian().strftime("%a, %d %b %Y")}\n'
+                metadata.write(meta_string)
+
+        self.elements.clear()
+
+
     def run(self, days=1, channel='irinn'):
         try:
+
             today = jdatetime.date.today()
             for i in range(days):
                 date = today - jdatetime.timedelta(i)
                 self.logger.info(f'{date} of {channel}')
                 self.get_link_per_channel_date(date, channel)
-                self.write_to_file()
+                self.write_links()
+                self.write_metadata()
+
         except ChannelDoesNotExistException as e:
             self.logger.error(f'{e}', exc_info=True)
         except KeyboardInterrupt:
@@ -173,10 +238,12 @@ class TelewebionScraper(webdriver.Firefox):
 
     def automatic_scrape(self, days=30):
         try:
+
             self.logger.info('Automatic Scrape Started.')
             for channel in tqdm(self.valid_channels):
                 self.logger.info(f'{days} days of {channel} is being scraped...')
                 self.run(days, channel)
+
         except KeyboardInterrupt:
             self.logger.debug('Program is exiting...')
             self.logger.debug('exit')
@@ -184,21 +251,37 @@ class TelewebionScraper(webdriver.Firefox):
             self.logger.debug('Browser closed.', exc_info=True)
 
     def download(self, quality='720'):
-        self.logger.info('Start Downloading...')
-        os.makedirs(f'./data/videos/{quality}', exist_ok=True)
         with open(f'./data/{quality}.txt', 'r') as f:
             links = f.readlines()
             links = list(map(lambda x: x[:-1], links))
         self.logger.info(f'{quality}p video links loaded.')
+        self.logger.info('Start Downloading...')
         try:
             for i in range(len(links)):
                 try:
                     self.logger.info(f'({i+1} of {len(links)})')
                     link = links[i]
+                    video_id = link.split('/')[-3]
                     filename = '-'.join(link.split('/')[-3:-1]) + '.mp4'
+
                     r = requests.get(link, allow_redirects=True, stream=True)
                     total = int(r.headers.get('content-length', 0))
-                    with open(f'./data/videos/{quality}/{filename}', 'wb') as f, tqdm(
+
+                    dir_name = f'./data/videos/{video_id}/{quality}'
+                    os.makedirs(dir_name, exist_ok=True)
+
+                    metadata_path = f'{dir_name}/{video_id}-meta.txt'
+                    with open(metadata_path, 'w') as metadata:
+                        meta_string = ''
+                        meta_string += f'Client Time: {datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")}\n'
+                        meta_string += f'Server Time: {r.headers.get("Date")}\n'
+                        meta_string += f'Video Last Modified: {r.headers.get("Last-Modified")}\n'
+                        meta_string += f'Video Size: {total} B\n'
+                        metadata.write(meta_string)
+                    self.logger.info(f'the metadata of video has written to {metadata_path}')
+
+                    file_path = f'{dir_name}/{filename}'
+                    with open(file_path, 'wb') as f, tqdm(
                         desc=filename,
                         total=total,
                         unit='iB',
@@ -208,6 +291,8 @@ class TelewebionScraper(webdriver.Firefox):
                         for data in r.iter_content(chunk_size=1024):
                             size = f.write(data)
                             bar.update(size)
+                    self.logger.info(f'video [{video_id}] has written to {file_path}')
+
                 except requests.exceptions.HTTPError as errh:
                     self.log.error(f"Http Error: {errh}", exc_info=True)
                 except requests.exceptions.ConnectionError as errc:
