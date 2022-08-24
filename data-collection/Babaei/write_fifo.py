e6e2a9f0d1cc7e8fbdae38077f0c512fd5d6e53b
AmirMohammad Babaei <iran.volleyball79@gmail.com>
2022-08-24 15:16:47 +0430

    write_fifo.py file deleted

--- data-collection/Babaei/write_fifo.py
+++
@@ -1,6 +0,0 @@
-from scraper.telewebion import TelewebionScraper
-from getpass import getuser
-with TelewebionScraper() as tele_obj:
-    tele_obj.quit()
-    tele_obj.logger.info(f'current user: {getuser()}')
-    tele_obj.write_links(isPipe=True)
