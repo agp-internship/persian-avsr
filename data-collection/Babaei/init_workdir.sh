b9dd63a1ef64fda5d1ca172957a3aa08b1b3a848
AmirMohammad Babaei <iran.volleyball79@gmail.com>
2022-08-24 17:05:56 +0430

    add init shell script

---
+++ data-collection/Babaei/init_workdir.sh
@@ -0,0 +1,5 @@
+mkdir log                       && \
+mkdir geckodriver               && \
+wget https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz -O ./geckodriver/geckodriver.tar.gz && \
+tar -zxf ./geckodriver/geckodriver.tar.gz -C ./geckodriver/ && \
+chmod +x geckodriver
